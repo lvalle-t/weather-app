bdaa0e748dbed1dbbb6ea3f37caf207e638cdd97
lvalle-t <lupitavalle713@gmail.com>
2023-08-04 00:37:33 -0500

    In progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e1b1cd7751a6a6a7c2ee64cf0868fb08'
-
 import React, { useState } from 'react'
 import axios from 'axios'
+const API_KEY = process.env.REACT_APP_API_KEY
+
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
+  
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e1b1cd7751a6a6a7c2ee64cf0868fb08`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
+  
+  // const [temp, setTemp] = useState(data.main.temp);
+  const [unit, setUnit] = useState("C");
+  const oppositeUnit = unit === "C" ? "F" : "C";
+  const convert = () => {
+    if (unit === "C") {
+      // const newT = temp * 1.8 + 32;
+      // setTemp(Math.round(newT));
+      setUnit(oppositeUnit);
+    }
 
+    if (unit === "F") {
+      // const newT = ((temp - 32) * 5) / 9;
+      // setTemp(Math.round(newT));
+      setUnit(oppositeUnit);
+    }
+  };
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -20,6 +37,8 @@ function App() {
     }
   }
 
+
+
   return (
     <div className="app">
       <div className="search">
@@ -29,14 +48,17 @@ function App() {
           onKeyPress={searchLocation}
           placeholder='Enter Location'
           type="text" />
+          <button onClick={convert}>°{oppositeUnit}</button>
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
           </div>
+          {/* <img className='weather-icon'lassName='weather-icon' alt='weather-icon' src={require('./assets/clear.png')}/> */}
+          {data.main ? <img  className='weather-icon' alt='weather-icon' src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}/> : null}
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°{unit}</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -45,8 +67,8 @@ function App() {
 
         {data.name !== undefined &&
           <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+             <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°{unit}</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
@@ -59,12 +81,12 @@ function App() {
             </div>
           </div>
         }
-
-
-
       </div>
+
     </div>
+    
   );
+  
 }
 
 export default App;
