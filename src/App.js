0a341e642c1bb8d104c84cb599fe10e38e30b01a
lvalle-t <lupitavalle713@gmail.com>
2023-08-05 22:51:49 -0500

    weather-app progress

--- src/App.js
+++ src/App.js
@@ -7,40 +7,50 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
   
+  // const weatherurl = `https://api.openweathermap.org//data/2.5/forecast?q=${location}&cnt=5&appid=${API_KEY}&units=imperial`
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
-  
-  // const [temp, setTemp] = useState(data.main.temp);
-  const [unit, setUnit] = useState("C");
+  const weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
+  const [temps, setTemp] = useState(null); // Initialize the temperature state
+  const [unit, setUnit] = useState("F");
+  const [feelsLike, setFeelsLike] = useState(null); // Initialize the "feels-like" state
   const oppositeUnit = unit === "C" ? "F" : "C";
-  const convert = () => {
-    if (unit === "C") {
-      // const newT = temp * 1.8 + 32;
-      // setTemp(Math.round(newT));
-      setUnit(oppositeUnit);
-    }
-
-    if (unit === "F") {
-      // const newT = ((temp - 32) * 5) / 9;
-      // setTemp(Math.round(newT));
-      setUnit(oppositeUnit);
-    }
-  };
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
+      axios.get(weatherurl).then((response) => {
         setData(response.data)
+        setTemp(response.data.main.temp); // Store the temperature value in the state
+        setFeelsLike(response.data.main.feels_like); // Store the "feels-like" temperature value
         console.log(response.data)
       })
       setLocation('')
     }
   }
 
+  const convert = () => {
+    if (unit === "C") {
+      const newT = temps * 1.8 + 32;
+      setTemp(Math.round(newT));
+      
+      const newFeelsLike = feelsLike * 1.8 + 32; // Convert the "feels-like" temperature to Fahrenheit
+      setFeelsLike(Math.round(newFeelsLike));
+      setUnit(oppositeUnit);
+    }
+
+    if (unit === "F") {
+      const newT = ((temps - 32)/1.8);
+      setTemp(Math.round(newT));
+
+      const newFeelsLike = (feelsLike - 32) / 1.8; // Convert the "feels-like" temperature to Celsius
+      setFeelsLike(Math.round(newFeelsLike));
+      setUnit(oppositeUnit);
+    }
+  };
 
 
   return (
     <div className="app">
+{/*/// search bar// */}
       <div className="search">
         <input
           value={location}
@@ -50,7 +60,9 @@ function App() {
           type="text" />
           <button onClick={convert}>°{oppositeUnit}</button>
       </div>
+
       <div className="container">
+{/*//// top//// */}
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
@@ -58,17 +70,20 @@ function App() {
           {/* <img className='weather-icon'lassName='weather-icon' alt='weather-icon' src={require('./assets/clear.png')}/> */}
           {data.main ? <img  className='weather-icon' alt='weather-icon' src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}/> : null}
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°{unit}</h1> : null}
+            {data.main ? <h1>{temps.toFixed()}°{unit}</h1> : null}
+            {/* {data.main ? <h1>{data.main.temp.toFixed()}°{unit}</h1> : null} */}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-
+      
+{/*/// bottom// */}
         {data.name !== undefined &&
           <div className="bottom">
              <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°{unit}</p> : null}
+             {feelsLike !== null ? <p className='bold'>{feelsLike.toFixed()}°{unit}</p> : null}
+              {/* {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°{unit}</p> : null} */}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
@@ -81,6 +96,18 @@ function App() {
             </div>
           </div>
         }
+
+        {/* {data.name !== undefined &&(
+          <div className="bottom-forecast">
+            <div className= "forecast"> 
+              Friday
+              <img className= "forecast-icon"alt="forecast-icon" src={require("./assets/clear.png")}/>
+              83°F<br></br>68°F
+            </div>
+            
+          </div>
+        )} */}
+        
       </div>
 
     </div>
