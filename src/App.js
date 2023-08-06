b3ef3474c5a8a135214ddb2c74bfaa9251acf319
lvalle-t <lupitavalle713@gmail.com>
2023-08-06 01:07:30 -0500

    weather-app

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
   
-  // const weatherurl = `https://api.openweathermap.org//data/2.5/forecast?q=${location}&cnt=5&appid=${API_KEY}&units=imperial`
+  const weatherurl = `https://api.openweathermap.org//data/2.5/forecast?q=${location}&cnt=5&appid=${API_KEY}&units=imperial`
 
-  const weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
-  const [temps, setTemp] = useState(null); // Initialize the temperature state
+  // const weatherurl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
+  const [temps, setTemp] = useState(null); 
   const [unit, setUnit] = useState("F");
-  const [feelsLike, setFeelsLike] = useState(null); // Initialize the "feels-like" state
+  const [feelsLike, setFeelsLike] = useState(null); 
   const oppositeUnit = unit === "C" ? "F" : "C";
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(weatherurl).then((response) => {
         setData(response.data)
-        setTemp(response.data.main.temp); // Store the temperature value in the state
-        setFeelsLike(response.data.main.feels_like); // Store the "feels-like" temperature value
+        setTemp(response.data.list[0].main.temp); 
+        setFeelsLike(response.data.list[0].main.feels_like); 
         console.log(response.data)
       })
       setLocation('')
@@ -32,7 +32,7 @@ function App() {
       const newT = temps * 1.8 + 32;
       setTemp(Math.round(newT));
       
-      const newFeelsLike = feelsLike * 1.8 + 32; // Convert the "feels-like" temperature to Fahrenheit
+      const newFeelsLike = feelsLike * 1.8 + 32;
       setFeelsLike(Math.round(newFeelsLike));
       setUnit(oppositeUnit);
     }
@@ -41,7 +41,7 @@ function App() {
       const newT = ((temps - 32)/1.8);
       setTemp(Math.round(newT));
 
-      const newFeelsLike = (feelsLike - 32) / 1.8; // Convert the "feels-like" temperature to Celsius
+      const newFeelsLike = (feelsLike - 32) / 1.8; 
       setFeelsLike(Math.round(newFeelsLike));
       setUnit(oppositeUnit);
     }
@@ -65,48 +65,73 @@ function App() {
 {/*//// top//// */}
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            <p>{data.city ? data.city.name : ''}</p>
           </div>
-          {/* <img className='weather-icon'lassName='weather-icon' alt='weather-icon' src={require('./assets/clear.png')}/> */}
-          {data.main ? <img  className='weather-icon' alt='weather-icon' src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}/> : null}
+          {data.list && data.list[0] && data.list[0].weather ? (
+            <img  
+              className='weather-icon' 
+              alt='weather-icon' 
+              src={`https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`}
+              /> 
+            ): null
+          }
           <div className="temp">
-            {data.main ? <h1>{temps.toFixed()}°{unit}</h1> : null}
-            {/* {data.main ? <h1>{data.main.temp.toFixed()}°{unit}</h1> : null} */}
+            {data.list ? <h1>{temps.toFixed()}°{unit}</h1> : null}
           </div>
           <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.list && data.list[0] && data.list[0].weather ? (
+              <p>{data.list[0].weather[0].main}</p>
+            ) : null}
           </div>
         </div>
       
 {/*/// bottom// */}
-        {data.name !== undefined &&
+
+        {data.list !== undefined &&(
           <div className="bottom">
              <div className="feels">
              {feelsLike !== null ? <p className='bold'>{feelsLike.toFixed()}°{unit}</p> : null}
-              {/* {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°{unit}</p> : null} */}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              {data.list && data.list[0] && data.list[0].main ? (
+                <p className='bold'>{data.list[0].main.humidity}%</p> 
+              ): null}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.list && data.list[0] && data.list[0].wind ? (
+                <p className="bold">{data.list[0].wind.speed.toFixed()} MPH</p>
+              ) : null}              
               <p>Wind Speed</p>
             </div>
           </div>
-        }
+        )}
 
-        {/* {data.name !== undefined &&(
+        {data.list !== undefined && (
           <div className="bottom-forecast">
-            <div className= "forecast"> 
-              Friday
-              <img className= "forecast-icon"alt="forecast-icon" src={require("./assets/clear.png")}/>
-              83°F<br></br>68°F
-            </div>
-            
+            {data.list.slice(1).map((forecast, index) => (
+              <div className="forecast" key={index}>
+                {forecast.dt_txt.split(' ')[1]}
+                {forecast.weather ? (
+                  <img
+                    className="forecast-icon"
+                    alt="forecast-icon"
+                    src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+                  />
+                ) : null}
+                {forecast.main ? (
+                  // chnage to min and max or chnage to temp and feels like?
+                  <>
+                    {unit === 'C' ? Math.round((forecast.main.temp - 32) / 1.8) : Math.round(forecast.main.temp)}°{unit}
+                    <br />
+                    {unit === 'C' ? Math.round((forecast.main.feels_like - 32) / 1.8) : Math.round(forecast.main.feels_like)}°{unit}
+                  </>
+                ) : null}
+              </div>
+            ))}
           </div>
-        )} */}
+        )}
         
       </div>
 
